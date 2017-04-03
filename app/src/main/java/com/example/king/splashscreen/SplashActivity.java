0c361a9e3760cdebcdfa8f7e2dbd5d6ea800b044
guapusing <rendisukirman.rs@gmail.com>
2017-04-03 13:42:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/king/splashscreen/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.king.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+/**
+ * Created by king on 3/21/2017.
+ */
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
